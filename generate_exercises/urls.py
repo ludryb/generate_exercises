1e06ce09ca5eb48a00a0d0004c9079bf4e8b21e8
Mila <djscaredo@rambler.ru>
2023-07-12 20:21:48 +0300

    changing project name

--- generate_exercises/urls.py
+++ generate_exercises/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for exer_proj project.
+URL configuration for generate_exercises project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- generate_exercises/wsgi.py
+++ generate_exercises/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exer_proj project.
+WSGI config for generate_exercises project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exer_proj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "generate_exercises.settings")
 
 application = get_wsgi_application()
