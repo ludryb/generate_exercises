a9d3e3f8c332834dcbb29ec3d535f367b8a75444
Mila <djscaredo@rambler.ru>
2023-08-16 12:08:44 +0300

    key&debug

--- generate_exercises/settings.py
+++ generate_exercises/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+0441lywjnj8$bsh7ck)-c39!1wo)xr9qmtm!_5i*g^n*wztnf"
+SECRET_KEY = "django-insecure-+0441lywjnj8$bsh7ck)-c39!1wo)xr9qmtm!_5i*g^n*wztnfvblf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 APPEND_SLASH = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 CSRF_COOKIE_SECURE = False
